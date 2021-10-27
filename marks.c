5ec5172451c3cac3b87f5366a899487dfd316c42
sunkaranaveenkalyan <92291249+sunkaranaveenkalyan@users.noreply.github.com>
2021-10-27 12:31:34 +0530

    Add files via upload

---
+++ marks.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+void main()
+{
+	int phy,chem,math,eng,tot;
+	phy=50;
+	chem=60;
+	math=70;
+	eng=40;
+	tot=phy+chem+math+eng;
+	printf("marks in physics:%d\nmarks in chemistry:%d\nmarks in maths:%d\nmarks in english:%d\ntotal marks:%d",phy,chem,math,eng,tot);
+}
