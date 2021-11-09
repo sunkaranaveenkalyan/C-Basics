d3216999f1ec8380caa0f88726a6557cbe5f4f92
sunkaranaveenkalyan <92291249+sunkaranaveenkalyan@users.noreply.github.com>
2021-11-09 12:29:53 +0530

    Add files via upload

---
+++ log op.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter three numbers");
+	scanf("%d%d%d",&a,&b,&c);
+	/*printf("enter 1st number");
+	scanf("%d",&a);
+	printf("enter 2nd number");
+	scanf("%d",&b);
+	printf("enter 3rd number");		
+    scanf("%d",&c);     */
+    printf("number 1:%d\n",a);
+    printf("number 2:%d\n",b);
+    printf("number 3:%d",c);
+
+
+}
