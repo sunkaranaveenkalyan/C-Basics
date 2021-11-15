51a0a8da92e0af6d6211fca466ddf613235dea16
sunkaranaveenkalyan <92291249+sunkaranaveenkalyan@users.noreply.github.com>
2021-11-15 12:34:30 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter a character (a-z or A-Z):");
+	scanf("%d",&a);
+	printf("Corresponding ASCII character is %c is %d",a,a);
+	printf("Enter a character (a-z or A-Z):");
+    scanf("%d",&b);
+	printf("Corresponding ASCII character is %d is %c",b,b);
+	
+	
+	
+	
+	
+}
