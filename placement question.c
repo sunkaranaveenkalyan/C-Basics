7ad27b519dd12bf440d9cb6e7f34ac1c1785ea5c
sunkaranaveenkalyan <92291249+sunkaranaveenkalyan@users.noreply.github.com>
2021-12-14 12:24:29 +0530

    Add files via upload

---
+++ placement question.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int p,q,r;
+	p=3;
+	q=7;
+	r=8;
+	p=(6+5)&p;
+	if((q-3)<(3+q))
+	   
+	   	q=q+p;
+	   
+	   	if((7^r^q)<(4+q+p))
+	   	
+	   		r=(12&4)&r;
+	   		r=10&r;
+		   
+	 printf("%d",p+q+r);
+	   
+}
